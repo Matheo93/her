074ad3410010008dc0e57df4da993587692d5e63
Matheo93 <matheo93@users.noreply.github.com>
2026-01-13 00:11:33 +0000

    auto-backup 2026-01-13 00:11:33

--- frontend/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { VideoCall } from './video-call';
-export { InterruptibleVoice } from './interruptible-voice';
-export { RealtimeVoiceCall } from './realtime-voice-call';
-export { useVAD } from './use-vad';
